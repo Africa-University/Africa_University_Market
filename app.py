c37a7c984e1c286e4c91e559664952423462b365
Evas-diary <Evadengende@gmail.com>
2026-07-20 10:36:52 +0200

    Prepare application for production deployment

--- app.py
+++ app.py
@@ -750,4 +750,6 @@ def delete_product(product_id):
 # RUN APPLICATION
 # =========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port=int(os.environ.get("PORT", 5000 )) 
+    app.run(host="0.0.0.0", port=port, debug=True)
+
